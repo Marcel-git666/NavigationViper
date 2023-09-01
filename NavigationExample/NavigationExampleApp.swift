b9665cfe2bd480269b138ca73b355f4d3c65426f
Marcel Mravec <marcel.mravec@cleverlance.com>
2023-09-01 21:40:31 +0200

    Initial Commit

---
+++ NavigationExample/NavigationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationExampleApp.swift
+//  NavigationExample
+//
+//  Created by Marcel Mravec on 01.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
